adbc5b07ca2cdc66943c80a39d460067635ab94b
ashutoshpith <amxdp18@gmail.com>
2022-09-11 05:18:09 +0530

    send to topic layer added

--- apps/play/src/play.resolver.ts
+++ apps/play/src/play.resolver.ts
@@ -1,23 +1,30 @@
 import { KafkaProducer } from '@core/core/kafka/producer';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Message } from '@nestjs/microservices/external/kafka.interface';
 
+export class SendDataToTopic {
+  constructor(
+    public value: any,
+    public key: string = SendDataToTopic.name,
+    public partition: number = 0,
+    public headers: object = { trigger: SendDataToTopic.name },
+  ) {
+    this.value = JSON.stringify(this.value);
+  }
+}
+
+export class PlayEvent {
+  constructor(public name: string, public data: string) {}
+}
 @Resolver()
 export class PlayResolver {
   constructor(private readonly kclient: KafkaProducer) {}
 
   @Query(() => Boolean)
   hit(@Args('data') data: string) {
-    this.kclient
-      .emit('topic_play', {
-        key: 'me',
-        value: data,
-        partition: 2,
-        headers: {
-          ashvau: 'ashutoshpith',
-        },
-      } as Message)
-      .subscribe();
+    this.kclient.emit(
+      'topic_play',
+      new SendDataToTopic(new PlayEvent('ashutosh', data)),
+    );
 
     return true;
   }
